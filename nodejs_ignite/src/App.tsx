a9985f2505ebdf7ce65c1bdc594c20c7c18062c2
Gustavo Keidel <gustavokeidel@gmail.com>
2022-06-22 15:03:56 -0300

    quarta

--- nodejs_ignite/src/App.tsx
+++ nodejs_ignite/src/App.tsx
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-     <h1>Hello Vite + React e tambem aksdajjIgnite</h1>
+     <h1 className="text-4xl text-violet-500">Hello Vite + React e tambem Ignite</h1>
  )}
 
 export default App
